0ae0581117625882d43239bd2f60f600c9c47247
islam <nudinovislam4@gmail.com>
2022-02-27 22:07:19 +0600

    пишуться модели

--- backend/models.py
+++ backend/models.py
@@ -28,7 +28,19 @@ class IndexSlider1(models.Model):
         verbose_name_plural = "Что я должен делать"
 
 
+class CategoryDirection(models.Model):
+    name = models.CharField(max_length=70, verbose_name="Наименование категории")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категории напровлении"
+        verbose_name_plural = "Категории напровлении"
+
+
 class Direction(models.Model):
+    category = models.ForeignKey(CategoryDirection, verbose_name="Категория", on_delete=models.CASCADE)
     img = models.ImageField(verbose_name="Фото", upload_to="img_direction")
     title = models.CharField(max_length=50, verbose_name="Заголовок")
     description = models.TextField(verbose_name="Описание")
@@ -39,3 +51,16 @@ class Direction(models.Model):
     class Meta:
         verbose_name_plural = "Направления"
         verbose_name = "Направления"
+
+
+class AboutUs(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Заголовок")
+    text = RichTextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "О нас"
+        verbose_name = "О нас"
+
