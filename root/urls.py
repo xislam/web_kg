ec963b7868a033872ee854b548c1bd09bac20070
islam <nudinovislam4@gmail.com>
2022-03-03 14:53:42 +0600

    Создан блок настроин

--- root/urls.py
+++ root/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from backend.views import BlogList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('blocks/', BlogList.as_view(), name='blocks')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
