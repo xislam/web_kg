9bbf840e32ea855835288febce33df7e53394a44
Taalai <t2.kg@mail.ru>
2022-03-03 16:39:26 +0600

    add new version2

--- static-web/js/app.js
+++ static-web/js/app.js
@@ -26,6 +26,24 @@
                     }
    
     });
+    // @media (max-width: "400px") {
+ // 
+
+      // slidesPerView: 1,
+         // loop: true,
+    
+      // autoplay:{
+         // delay: 1000,
+         // disableOnInteraction: false,
+                    // },
+      // pagination: {
+        // el: '.swiper-pagination',
+        // clickable: true,
+      // },
+   
+    
+// } ;
+  
 
         var swiper = new Swiper('.swiper3', {
       slidesPerView: 3,
@@ -38,4 +56,57 @@
   },
    
     });
+        
+
+        var swiper = new Swiper('.swiper4', {
+      slidesPerView: 3,
+    
+      loop: true,
+    
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+  },
+        autoplay:{
+         delay: 5000,
+         disableOnInteraction: false,
+                    }
+   
+    });
+            var swiper = new Swiper('.swiper5', {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+   
+      autoplay:{
+         delay: 5000,
+         disableOnInteraction: false,
+                    }
+   
+    });
+
+            var swiper = new Swiper('.swiper6', {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+   
+      autoplay:{
+         delay: 5000,
+         disableOnInteraction: false,
+                    }
+   
+    });
+            var swiper = new Swiper('.swiper7', {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+   
+      autoplay:{
+         delay: 5000,
+         disableOnInteraction: false,
+                    }
+   
+    });
+            
+        
         
