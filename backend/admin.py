f3cc81b8add08270ab2edf4023e6b11a13da4f93
islam <nudinovislam4@gmail.com>
2022-03-18 02:23:25 +0600

    for pull admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from backend.models import IndexText, IndexSlider1, Direction, CategoryDirection, Contact, Blog, Gallery, AboutUs, Links
+from backend.models import IndexText, IndexSlider1, Direction,\
+    CategoryDirection, Contact, Blog, Gallery, AboutUs, Links, AboutKg
 
 admin.site.register(IndexText)
 admin.site.register(IndexSlider1)
@@ -10,5 +11,6 @@ admin.site.register(Contact)
 admin.site.register(Blog)
 admin.site.register(Gallery)
 admin.site.register(AboutUs)
+admin.site.register(AboutKg)
 admin.site.register(Links)
 admin.site.site_header = 'KYRGYZ LAND  ADMIN'
